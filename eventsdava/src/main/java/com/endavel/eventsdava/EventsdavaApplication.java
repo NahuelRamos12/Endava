03c77c80e49f307dd462bd6dd89f9070657d69c5
Nahuel Ramos Veron <Nahuel.RamosVeron@endava.com>
2022-04-29 11:34:20 -0300

    eventsdava first commit

---
+++ eventsdava/src/main/java/com/endavel/eventsdava/EventsdavaApplication.java
@@ -0,0 +1,13 @@
+package com.endavel.eventsdava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsdavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsdavaApplication.class, args);
+	}
+
+}
---
+++ eventsdava/src/test/java/com/endavel/eventsdava/EventsdavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endavel.eventsdava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsdavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
