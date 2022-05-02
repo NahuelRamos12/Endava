321f8a816c24e3eeea587c85c16d429552484dca
Carolina Yokoyama <Carolina.Yokoyama@endava.com>
2022-05-02 10:07:27 -0300

    added getAll and getByID in service

--- eventsdava/src/main/java/com/endavel/eventsdava/services/EventService.java
+++ eventsdava/src/main/java/com/endavel/eventsdava/services/EventService.java
@@ -1,5 +1,33 @@
 package com.endavel.eventsdava.services;
 
-public class EventService {
+import java.util.List;
+import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.endavel.eventsdava.domain.Event;
+import com.endavel.eventsdava.persistance.EventsRepository;
+import com.endavel.eventsdava.services.exceptions.EventNotFoundException;
+
+@Service
+public class EventService implements IEventService {
+
+	private EventsRepository eventsRepository;
+	
+	@Autowired
+	public void setClientRepository(EventsRepository eventsRepository) {
+		this.eventsRepository = eventsRepository;
+	}
+
+	@Override
+	public List<Event> getAllEvents() {
+		return (List<Event>) eventsRepository.findAll();
+	}
+	
+	@Override
+	public Event getEventById(UUID id) throws EventNotFoundException {		
+		return eventsRepository.findById(id).orElseThrow(EventNotFoundException::new);
+	}
+	
 }
---
+++ eventsdava/src/main/java/com/endavel/eventsdava/services/exceptions/EventNotFoundException.java
@@ -0,0 +1,5 @@
+package com.endavel.eventsdava.services.exceptions;
+
+public class EventNotFoundException extends RuntimeException{
+
+}
