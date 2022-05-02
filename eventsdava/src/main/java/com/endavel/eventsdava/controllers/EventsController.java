3f12744cd42daaea5d3de7f9d00d2d526d26b747
NRamos <NRamos@EN1710349.endava.net>
2022-05-02 10:15:48 -0300

    Eventsdava update

--- eventsdava/src/main/java/com/endavel/eventsdava/controllers/EventsController.java
+++ eventsdava/src/main/java/com/endavel/eventsdava/controllers/EventsController.java
@@ -1,6 +1,7 @@
 package com.endavel.eventsdava.controllers;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,18 +15,23 @@ import com.endavel.eventsdava.services.IEventService;
 @RestController
 @RequestMapping("/api/eventsdava") 
 public class EventsController {
+	private IEventService eventService;
+	
+	public EventsController(IEventService eventService) {
+		this.eventService = eventService;
+	}
 	
 	@GetMapping
     public ResponseEntity<List<Event>> getAllEvents(){
-		return ResponseEntity.ok(IEventService.getAllEvents());
+		return ResponseEntity.ok(eventService.getAllEvents());
     }
 //	@GetMapping
 //    public ResponseEntity<List<Event>> getEventByType(){
 //		return ResponseEntity.ok(IEventService.getAllEvents());
 //    }
 	@GetMapping("/{id}")
-    public ResponseEntity<Event> getEventById(@PathVariable int id){
-		return ResponseEntity.ok(IEventService.getEventById());
+    public ResponseEntity<Event> getEventById(@PathVariable UUID id){
+		return ResponseEntity.ok(eventService.getEventById(id));
     }
 	
 	
--- eventsdava/src/main/java/com/endavel/eventsdava/domain/Event.java
+++ eventsdava/src/main/java/com/endavel/eventsdava/domain/Event.java
@@ -24,6 +24,7 @@ public class Event {
 	private String location;
 	private String description;
 	
+	public Event() {}
 	
 	public UUID getIdEvent() {
 		return idEvent;
